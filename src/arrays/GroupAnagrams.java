61995f28bb7d16f5798904b877cbdd9061fcebec
Ishannilawar2002 <ishannilawar2002@gmail.com>
2026-09-12 15:37:24 +0530

    solve: Group Anagrams (HashMap + sorted-string fingerprint, O(n*k log k) time)

---
+++ src/arrays/GroupAnagrams.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
